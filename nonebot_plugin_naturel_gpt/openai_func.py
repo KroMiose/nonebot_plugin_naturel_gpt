62b0c97d94ee737c4146744bd5289138d0be226b
KroMiose <li_xiangff@163.com>
2023-03-11 16:10:50 +0800

    * 修复bot回复开始以 bot_name：(中文冒号)开头时不自动去除的问题

--- nonebot_plugin_naturel_gpt/openai_func.py
+++ nonebot_plugin_naturel_gpt/openai_func.py
@@ -95,6 +95,9 @@ class TextGenerator(Singleton["TextGenerator"]):
                 # 去掉可能存在的开头起始标志
                 if res.startswith(f"{custom.get('bot_name', 'AI')}:"):
                     res = res[len(f"{custom.get('bot_name', 'AI')}:"):]
+                # 去掉可能存在的开头起始标志 (中文)
+                if res.startswith(f"{custom.get('bot_name', 'AI')}："):
+                    res = res[len(f"{custom.get('bot_name', 'AI')}："):]
                 # 替换多段回应中的回复起始标志
                 res = res.replace(f"\n\n{custom.get('bot_name', 'AI')}:", "*;")
             else:
