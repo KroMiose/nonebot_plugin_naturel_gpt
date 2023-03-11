d83798fe6b1e1de6c1aabf3b35fb65720cd381b0
Misaka Mikoto <easy66@live.com>
2023-03-11 18:49:49 +0800

    * 配置文件数据类型由字典改为类

--- nonebot_plugin_naturel_gpt/__init__.py
+++ nonebot_plugin_naturel_gpt/__init__.py
@@ -16,7 +16,7 @@ from .persistent_data_manager import PersistentDataManager
 from . import matcher
 
 
-global_data_path = f"{config['NG_DATA_PATH']}naturel_gpt.pkl"
+global_data_path = f"{config.NG_DATA_PATH}naturel_gpt.pkl"
 
 def set_permission_check_func(callback:Callable[[Matcher, MessageEvent, Bot, str, str], Awaitable[Tuple[bool,str]]]):
     """设置Matcher的权限检查函数"""
@@ -29,27 +29,28 @@ set_permission_check_func(utils.identity_mofify_check)
 PersistentDataManager.instance.load_from_file(global_data_path)
 
 # 读取ApiKeys
-api_keys = config['OPENAI_API_KEYS']
+api_keys = config.OPENAI_API_KEYS
 logger.info(f"共读取到 {len(api_keys)} 个API Key")
 
 # 检查聊天摘要功能是否开启 未开启则清空所有聊天摘要
-if not config.get('CHAT_ENABLE_SUMMARY_CHAT'):
+if not config.CHAT_ENABLE_SUMMARY_CHAT:
     logger.warning("聊天摘要功能已关闭，将自动清理历史聊天摘要数据")
     PersistentDataManager.instance.clear_all_chat_summary()
 
 """ ======== 初始化对话文本生成器 ======== """
 TextGenerator.instance.init(api_keys=api_keys, config={
-        'model': config['CHAT_MODEL'],
-        'max_tokens': config['REPLY_MAX_TOKENS'],
-        'temperature': config['CHAT_TEMPERATURE'],
-        'top_p': config['CHAT_TOP_P'],
-        'frequency_penalty': config['CHAT_FREQUENCY_PENALTY'],
-        'presence_penalty': config['CHAT_PRESENCE_PENALTY'],
-        'max_summary_tokens': config['CHAT_MAX_SUMMARY_TOKENS'],
-        'timeout': config['OPENAI_TIMEOUT'],
+        'model': config.CHAT_MODEL,
+        'max_tokens': config.REPLY_MAX_TOKENS,
+        'temperature': config.CHAT_TEMPERATURE,
+        'top_p': config.CHAT_TOP_P,
+        'frequency_penalty': config.CHAT_FREQUENCY_PENALTY,
+        'presence_penalty': config.CHAT_PRESENCE_PENALTY,
+        'max_summary_tokens': config.CHAT_MAX_SUMMARY_TOKENS,
+        'timeout': config.OPENAI_TIMEOUT,
 }, 
-proxy=config['OPENAI_PROXY_SERVER'] if config.get('OPENAI_PROXY_SERVER') else None # 代理服务器配置
+proxy=config.OPENAI_PROXY_SERVER if config.OPENAI_PROXY_SERVER else None # 代理服务器配置
 )
 
 """ ======== 加载拓展模块 ======== """
-load_extensions(config)
+# Extension 模块有作为 __main__ 执行的需求，此时无法加载 class Config, 因此需要传递字典
+load_extensions(dict(config))
