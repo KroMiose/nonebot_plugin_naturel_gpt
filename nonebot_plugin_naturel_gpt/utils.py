1fe7750973c51d87cf63fdb95605425897535598
Misaka Mikoto <easy66@live.com>
2023-03-13 02:16:50 +0800

    * 预处理消息，解析其中的 at 片段为真实名字

--- nonebot_plugin_naturel_gpt/utils.py
+++ nonebot_plugin_naturel_gpt/utils.py
@@ -5,6 +5,7 @@ from nonebot.adapters.onebot.v11 import Bot, MessageEvent
 from nonebot.rule import Rule
 from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
+from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent, PrivateMessageEvent, GroupMessageEvent, MessageSegment, GroupIncreaseNoticeEvent
 
 from .config import *
 
@@ -19,7 +20,7 @@ def to_me():
 
     return Rule(_to_me)
 
-async def identity_mofify_check(matcher:Matcher, event: MessageEvent, bot:Bot, cmd:str, type:str = 'cmd') -> Tuple[bool, str]:
+async def default_permission_check_func(matcher:Matcher, event: MessageEvent, bot:Bot, cmd:str, type:str = 'cmd') -> Tuple[bool, str]:
     """默认权限检查函数"""
     if cmd is None:
         return (True, None)
@@ -49,3 +50,27 @@ async def identity_mofify_check(matcher:Matcher, event: MessageEvent, bot:Bot, c
     else:
         return (True, None)
     
+async def gen_chat_text(event: MessageEvent, bot:Bot) -> str:
+    """生成合适的会话消息内容(eg. 将cq at 解析为真实的名字)"""
+    if not isinstance(event, GroupMessageEvent):
+        return event.get_plaintext()
+    else:
+        msg = ''
+        for seg in event.message:
+            if seg.is_text():
+                msg += seg.data.get('text', '')
+            elif seg.type == 'at':
+                qq = seg.data.get('qq', None)
+                if qq:    
+                    user_name = await get_user_name(event=event, bot=bot,user_id=int(qq))
+                    if user_name:
+                        msg += user_name # at segment 后面跟的消息前面一般会有个空格，不知保留是否对chatgpt对话有影响
+        return msg
+    
+async def get_user_name(event: MessageEvent, bot:Bot, user_id:int) -> str:
+    """获取QQ用户名，优先群名片"""
+    if isinstance(event, GroupMessageEvent):
+        user_info = await bot.get_group_member_info(group_id=event.group_id, user_id=user_id, no_cache=False)
+        return user_info.get('card', None) or user_info.get('nickname', None)
+    else:
+        return event.sender.nickname
