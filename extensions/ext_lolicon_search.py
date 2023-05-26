db5d5fa4331773abffc5d2dde769de2c1fd6ad5f
lgc2333 <lgc2333@126.com>
2023-05-27 07:32:15 +0800

    调

--- extensions/ext_lolicon_search.py
+++ extensions/ext_lolicon_search.py
@@ -19,9 +19,9 @@ ext_config = {
     "description": (
         "Get an anime image information and URL using Lolicon API. "
         "Use this extension when you want to get or send an anime picture. "
-        '(For example, You can get a random image by using "/#anime_pic_search&#/"; '
+        'For example, You can get a random image by using "/#anime_pic_search&#/"; '
         'And you can also get a image info in keywords ("可爱") AND ("白丝" OR "黑丝") '
-        'by using "/#anime_pic_search&可爱,白丝|黑丝#/" in your response. '
+        'by using "/#anime_pic_search&可爱,白丝|黑丝#/" in your response.'
     ),
     # 参考词，用于上下文参考使用，为空则每次都会被参考 (消耗 token)
     "refer_word": [],
